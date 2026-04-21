952bc5c49a52556632f9b668a5d3115e5ee13d3e
gabrielemarrocco08-prog <gabriele.marrocco08@gmail.com>
2026-04-21 16:20:25 +0200

    Add files via upload

---
+++ colonnina1.ino
@@ -0,0 +1,61 @@
+#include <ArduinoJson.h>
+#include <LiquidCrystal.h>
+
+#define PIN_POT A0
+#define pPOn 3
+
+String id_macchina = "C001";
+bool vPOn = false;
+int valore = 0;
+int percentuale = 0;
+
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
+void setup() {
+  Serial.begin(9600);
+
+  lcd.begin(16, 2);
+
+  pinMode(PIN_POT, INPUT);
+  pinMode(pPOn, INPUT_PULLUP);  // ✔ corretto
+}
+
+void loop() {
+
+  // Lettura potenziometro
+  valore = analogRead(PIN_POT);
+
+  // Lettura pulsante (invertita perché pullup)
+  vPOn =!digitalRead(pPOn);
+
+  // Percentuale
+  percentuale = map(valore, 0, 1023, 0, 100);
+
+  int clienti = random(0, 50);
+  int profitto = percentuale * 2;
+  int runtime = millis() / 1000;
+
+  StaticJsonDocument<200> doc;
+
+  doc["id_macchina"] = id_macchina;
+  doc["clienti_day"] = clienti;
+  doc["consumo"] = percentuale;
+  doc["profitto"] = profitto;
+  doc["runtime"] = runtime;
+
+  if (vPOn) {
+
+    // JSON pulito
+    serializeJsonPretty(doc, Serial);
+    Serial.println();
+
+    // LCD pulito
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Batteria:");
+
+    lcd.setCursor(0, 1);
+    lcd.print(percentuale);
+    lcd.print("%");
+  }
+}
